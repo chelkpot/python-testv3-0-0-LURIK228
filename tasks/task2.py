1ff04f2a4cae7a9746b2db32795d48de3d7cbf79
Admin <lurik228@yandex.ru>
2025-10-08 21:50:24 +0500

    сделал дз

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,10 +2,12 @@
 
 def solve():
 # Ниже пишите решение задачи
+    z=int(input())
+    z=z%1440
+    chas=z//60
+    minuta=z%60
+    print(chas, minuta)
 
-   
-
-   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
